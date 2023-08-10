13c4b39ad7f63b1df5c62911cd7df7dcf7d0d8f4
Emil Bohleber <emil.bohleber@brickmakers.de>
2023-08-10 19:55:42 +0200

    extract first version of compute shaders

---
+++ src/ExampleComputeShader.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+using Godot.Collections;
+using OceanWater.shader_system;
+
+namespace OceanWater;
+
+public class ExampleComputeShader : ComputeBase<byte[],byte[]>
+{
+    private Rid _buffer;
+    
+    public override ICompute<byte[], byte[]> InitInputs(byte[] input)
+    {
+        _buffer = Rd.StorageBufferCreate((uint)input.Length, input);
+        
+        var uniform = CreateUniform(RenderingDevice.UniformType.StorageBuffer, 0, _buffer);
+        
+        var uniformSet = Rd.UniformSetCreate(new Array<RDUniform> { uniform }, Shader, 0);
+        var pipeline = Rd.ComputePipelineCreate(Shader);
+        var computeList = Rd.ComputeListBegin();
+        Rd.ComputeListBindComputePipeline(computeList, pipeline);
+        Rd.ComputeListBindUniformSet(computeList, uniformSet, 0);
+        Rd.ComputeListDispatch(computeList, xGroups: 5, yGroups: 1, zGroups: 1);
+        Rd.ComputeListEnd();
+
+        return this;
+    }
+
+    public override byte[] ReadBack()
+    {
+        Rd.Sync();
+        return Rd.BufferGetData(_buffer);
+    }
+}
---
+++ src/shader_system/ICompute.cs
@@ -0,0 +1,57 @@
+﻿using Godot;
+
+namespace OceanWater.shader_system;
+
+public interface ICompute<TInput, TOutput>
+{
+    ICompute<TInput,TOutput> InitRenderingDevice();
+    ICompute<TInput,TOutput> InitShader(string path);
+    ICompute<TInput,TOutput> InitInputs(TInput input);
+    ICompute<TInput,TOutput> Run();
+    TOutput ReadBack();
+}
+
+public abstract class ComputeBase<TInput, TOutput> : ICompute<TInput, TOutput>
+{
+    protected RenderingDevice Rd;
+    protected Rid Shader;
+    
+    public ICompute<TInput,TOutput> InitRenderingDevice()
+    {
+        Rd = RenderingServer.CreateLocalRenderingDevice();
+        return this;
+    }
+
+    public ICompute<TInput,TOutput> InitShader(string path)
+    {
+        var shaderFile = GD.Load<RDShaderFile>(path);
+        var shaderByteCode = shaderFile.GetSpirV();
+        Shader = Rd.ShaderCreateFromSpirV(shaderByteCode);
+        return this;
+    }
+
+    public ICompute<TInput,TOutput> InitInputs()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public ICompute<TInput,TOutput> Run()
+    {
+        Rd.Submit();
+        return this;
+    }
+
+    protected RDUniform CreateUniform(RenderingDevice.UniformType uniformType, int binding, Rid buffer)
+    {
+        var uniform = new RDUniform
+        {
+            UniformType = uniformType,
+            Binding = binding,
+        };
+        uniform.AddId(buffer);
+        return uniform;
+    }
+    
+    public abstract ICompute<TInput,TOutput> InitInputs(TInput input);
+    public abstract TOutput ReadBack();
+}
